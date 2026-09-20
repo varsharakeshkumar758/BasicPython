8e7c60a17d39578cd737974f620b8309ffe64096
Varsha Rakesh Kumar <varsha.rakeshkumar758@education.nsw.gov.au>
2026-09-21 09:28:44 +1000

    Added comments and print author

--- program.py
+++ program.py
@@ -1,5 +1,6 @@
-import time
-for x in range(11):
-    print(10-x)
-    time.sleep(1)
-print("Takeoff!")
+print("written by Varsha")
+import time #adds time function
+for x in range (11): #for every number between 0 and 11
+    print(10-x) #print the 10 minus the number (so that it counts in reverse)
+    time.sleep(1) #delay 1 second
+print("Takeoff!") #when the countdown is done print Takeoff!
